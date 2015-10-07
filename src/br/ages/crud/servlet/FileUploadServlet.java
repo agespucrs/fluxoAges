a116bff5a076765e4a2532f8cf169c6ebd24d3b6
Max Zorzetti <maximilian.zorzetti@acad.pucrs.br>
2015-10-07 18:15:17 -0300

    2 - criação FileUploadServlet
    Signed-off-by: Max Zorzetti <maximilian.zorzetti@acad.pucrs.br>

---
+++ src/br/ages/crud/servlet/FileUploadServlet.java
@@ -0,0 +1,18 @@
+package br.ages.crud.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class FileUploadServlet {
+	
+	
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletRequest response){
+		
+	}
+	
+	protected void processRequest(HttpServletRequest request, HttpServletRequest response){
+		
+	}
+
+}
