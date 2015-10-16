64290341fc12c73d15f848d6402a0175445bb81f
DanieleSouza <danieleramosdesouza@gmail.com>
2015-10-16 16:28:50 -0300

    3 - Classe para teste do ProjetoDAO

---
+++ src/br/ages/crud/test/TestaProjetoDAO.java
@@ -0,0 +1,35 @@
+package br.ages.crud.test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestaProjetoDAO {
+	
+	@Before
+	public void init(){
+		
+
+	}	
+	
+	@Test
+	public void testCadastrarProjeto() {
+		fail("Not yet implemented");
+	}
+	
+	
+	
+	@Test
+	public void testListarProjetos() {
+		fail("Not yet implemented");
+	}
+
+
+
+	@Test
+	public void testRemoveProjeto() {
+		fail("Not yet implemented");
+	}
+
+}
