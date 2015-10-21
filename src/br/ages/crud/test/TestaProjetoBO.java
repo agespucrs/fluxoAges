33ebceb9a66540648dde5cf1180303ad63344a0b
Max Zorzetti <maximilian.zorzetti@acad.pucrs.br>
2015-10-21 18:18:40 -0200

    2 - criação da classe de Teste do ProjetoBO
    Signed-off-by: Max Zorzetti <maximilian.zorzetti@acad.pucrs.br>

---
+++ src/br/ages/crud/test/TestaProjetoBO.java
@@ -0,0 +1,51 @@
+package br.ages.crud.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.ages.crud.bo.ProjetoBO;
+import br.ages.crud.model.Equipe;
+import br.ages.crud.model.Projeto;
+import br.ages.crud.model.Stakeholder;
+import br.ages.crud.model.Status;
+
+public class TestaProjetoBO {
+	
+	private Projeto projetoCerto, projetoErrado;
+	private ProjetoBO projetoBO;
+	
+	
+
+	@Before
+	public void setUp() throws Exception {
+		projetoBO = new ProjetoBO();
+		ArrayList<Equipe> equipes = new ArrayList<Equipe>();
+		ArrayList<Stakeholder> stakeholders = new ArrayList<Stakeholder>();
+		Date dataInicio = new Date();
+		Date dataFim = new Date();
+		
+		
+		projetoCerto = new Projeto(10, "Projeto Correto", equipes, "Spipspops 2.0", Status.ATIVO , stakeholders, dataInicio, dataFim);
+		projetoErrado = new Projeto(-1, null, null, null, null, null, null, null);
+		
+		}
+
+	@Test
+	public void testValidarProjetoOK() {
+		boolean valido = projetoBO.validarProjeto(projetoCerto);
+		assertEquals(valido, true);
+	}
+	
+	@Test
+	public void testValidarProjetoNotOK(){
+		boolean valido = projetoBO.validarProjeto(projetoErrado);
+		assertEquals(valido, false);
+	}
+
+}
