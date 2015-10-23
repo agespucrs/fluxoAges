1301c10ec0d987e371f3a992dbf83a713c8378df
Caio Ilha <carlos.ilha@acad.pucrs.br>
2015-10-23 19:38:32 -0200

    4 - Carlos / Jo£o Corrç£o de acentos bugad£s

--- src/br/ages/crud/util/MensagemContantes.java
+++ src/br/ages/crud/util/MensagemContantes.java
@@ -2,25 +2,25 @@ package br.ages.crud.util;
 
 public class MensagemContantes {
 
-	public static final String MSG_ERR_CAMPO_OBRIGATORIO = "Campo ? obrigat�rio!";
+	public static final String MSG_ERR_CAMPO_OBRIGATORIO = "Campo ? obrigatório!";
 	
-	public static final String MSG_ERR_CAMPO_NOME_OBRIGATORIO = "Campo Nome obrigat�rio!";
+	public static final String MSG_ERR_CAMPO_NOME_OBRIGATORIO = "Campo Nome obrigatório!";
 
-	public static final String MSG_ERR_CAMPO_SEXO_OBRIGATORIO = "Campo Sexo obrigat�rio!";
+	public static final String MSG_ERR_CAMPO_SEXO_OBRIGATORIO = "Campo Sexo obrigatório!";
 	
-	public static final String MSG_ERR_CAMPO_CIDADE_OBRIGATORIO = "Campo Cidade obrigat�rio!";
+	public static final String MSG_ERR_CAMPO_CIDADE_OBRIGATORIO = "Campo Cidade obrigatório!";
 	
-	public static final String MSG_ERR_CAMPO_ESTADO_OBRIGATORIO = "Campo Estado obrigat�rio!";
+	public static final String MSG_ERR_CAMPO_ESTADO_OBRIGATORIO = "Campo Estado obrigatório!";
 	
-	public static final String MSG_ERR_CAMPO_LOGRADOURO_OBRIGATORIO = "Campo Logradouro obrigat�rio!";
+	public static final String MSG_ERR_CAMPO_LOGRADOURO_OBRIGATORIO = "Campo Logradouro obrigatório!";
 	
-	public static final String MSG_ERR_CAMPO_INVALIDO = "Campo ? inv�lido!";
+	public static final String MSG_ERR_CAMPO_INVALIDO = "Campo ? inválido!";
 	
-	public static final String MSG_ERR_USUARIO_SENHA_INVALIDOS = "Usu�rio/Senha inv�lidos!";
+	public static final String MSG_ERR_USUARIO_SENHA_INVALIDOS = "Usuário/Senha inválidos!";
 	
-	public static final String MSG_ERR_PESSOA_DADOS_INVALIDOS = "Dados da pessoa inv�lidos ou inconsistentes!";
+	public static final String MSG_ERR_PESSOA_DADOS_INVALIDOS = "Dados da pessoa inválidos ou inconsistentes!";
 
-	public static final String MSG_ERR_USUARIO_DADOS_INVALIDOS = "Dados do usu�rio inv�lidos ou inconsistentes!";
+	public static final String MSG_ERR_USUARIO_DADOS_INVALIDOS = "Dados do usuário inválidos ou inconsistentes!";
 	
 	public static final String MSG_ERR_CAMPO_CPF_MAIOR_RECOMENDADO = "Campo ? com mais de 11 caracteres!";
 	
@@ -34,20 +34,20 @@ public class MensagemContantes {
 	
 	public static final String MSG_SUC_CADASTRO_PESSOA = "Cadastro de Pessoa efetuado com sucesso!";
 	
-	public static final String MSG_SUC_CADASTRO_USUARIO = "Cadastro usu�rio: ?, efetuado com sucesso!";
+	public static final String MSG_SUC_CADASTRO_USUARIO = "Cadastro usuário: ?, efetuado com sucesso!";
 	
 	public static final String MSG_SUC_REMOVE_USUARIO= "Usuario: ?, removido com sucesso!";
 	
-	public static final String MSG_SUC_ATUALIZADA_PESSOA = "Atualiza��o de Pessoa efetuada com sucesso!";
+	public static final String MSG_SUC_ATUALIZADA_PESSOA = "Atualização de Pessoa efetuada com sucesso!";
 	
-	public static final String MSG_INF_LOGOUT = "Logout do Usuario efetuado com sucesso!";
+	public static final String MSG_INF_LOGOUT = "Logout do usuário efetuado com sucesso!";
 	
-	public static final String MSG_SENHA_INVALIDA = "A senha do usuario � invalida: Deve ter no minimo 3 e no maximo 8 caracteres, apenas numeros e letras!";
+	public static final String MSG_SENHA_INVALIDA = "A senha do usuario é inválida: Deve ter no mínimo 3 e no máximo 8 caracteres, apenas números e letras!";
 
 	//msgs de projeto
-	public static final String MSG_ERR_PROJETO_DADOS_INVALIDOS = "Dados DO projeto inv�lidos ou inconsistentes!";
+	public static final String MSG_ERR_PROJETO_DADOS_INVALIDOS = "Dados DO projeto inválidos ou inconsistentes!";
 		
-	public static final String MSG_ERR_PROJETO_ARQUIVO_INVALIDO = "Arquivo excede o ?Mb ou n�o � do tipo PDF";
+	public static final String MSG_ERR_PROJETO_ARQUIVO_INVALIDO = "Arquivo excede o ?Mb ou não é do tipo PDF";
 
 	public static final String MSG_ERR_MATRICULA_INVALIDA = "Dado de matrícula inválido";
 	
