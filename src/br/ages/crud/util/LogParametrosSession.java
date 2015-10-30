ff990016b102a2715261ef8501c7f7d72de78629
Cassio Trindade <cassiowt@gmail.com>
2015-10-30 15:11:58 -0200

    Adicionado log de parametros para o Servlet.
    Signed-off-by: Cassio Trindade <cassiowt@gmail.com>

---
+++ src/br/ages/crud/util/LogParametrosSession.java
@@ -0,0 +1,37 @@
+package br.ages.crud.util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogParametrosSession {
+
+	public static void logParametros(HttpServletRequest request) throws ServletException, IOException {
+		// Set response content type
+		
+		Enumeration paramNames = request.getParameterNames();
+
+		while (paramNames.hasMoreElements()) {
+			String paramName = (String) paramNames.nextElement();
+			System.out.println("Nome Parametro: " + paramName );
+			String[] paramValues = request.getParameterValues(paramName);
+			// Read single valued data
+			if (paramValues.length == 1) {
+				String paramValue = paramValues[0];
+				if (paramValue.length() == 0)
+					System.out.println("No Value>");
+				else
+					System.out.println("valor: " + paramValue);
+			} else {
+				// Read multiple valued data
+					for (int i = 0; i < paramValues.length; i++) {
+						System.out.println("valor("+i+"): "  + paramValues[i]);
+				}
+			}
+		}
+		}
+}
