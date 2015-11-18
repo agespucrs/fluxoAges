01031adb4177f44601a35683827104ab365a232f
VictorDiehl <15104347@w3070489.portoalegre.pucrsnet.br>
2015-11-18 19:58:09 -0200

    modaStakeholder criado e CreateScreenStakeholderCommand.java implementada.

--- src/br/ages/crud/command/CreateScreenStakeholderCommand.java
+++ src/br/ages/crud/command/CreateScreenStakeholderCommand.java
@@ -4,12 +4,34 @@ import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import br.ages.crud.bo.StakeholderBO;
+import br.ages.crud.model.Stakeholder;
+
 public class CreateScreenStakeholderCommand implements Command {
 
+	private String proxima;
+	
+	private StakeholderBO stakeholderBO;
 	@Override
 	public String execute(HttpServletRequest request) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		proxima= "main?acao=listStakeholder";
+		String isEdit = request.getParameter("isEdit");
+		try{
+		if (isEdit.equals("")) {
+		
+			proxima= "stakeholder/addStakeholder.jsp";
+			
+		} else {
+			int idStakeholder = Integer.parseInt(request.getParameter("idStakeholder"));
+			Stakeholder stakeholder = stakeholderBO.bucaStakeholderId(idStakeholder);
+			request.setAttribute("stakeholder", stakeholder);
+			proxima= "stakeholder/addStakeholder.jsp";
+		}
+		
+	} catch (Exception e) {
+		request.setAttribute("msgErro", e.getMessage());
 	}
-
+		
+	return proxima;
+  }
 }
