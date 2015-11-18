784165c1c161f95449bd0a110b7f5e2a10092ca5
Caio Ilha <carlos.ilha@acad.pucrs.br>
2015-11-18 19:05:56 -0200

    ListStakeholderCommand fazido

--- src/br/ages/crud/command/ListStakeholdersCommand.java
+++ src/br/ages/crud/command/ListStakeholdersCommand.java
@@ -1,15 +1,29 @@
 package br.ages.crud.command;
 
 import java.sql.SQLException;
-
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
-public class ListStakeholdersCommand implements Command {
+import br.ages.crud.bo.StakeholderBO;
+import br.ages.crud.exception.NegocioException;
+import br.ages.crud.model.Stakeholder;
 
+public class ListStakeholdersCommand implements Command {
+	private String proxima;
+	private StakeholderBO stakeholderBO;
 	@Override
 	public String execute(HttpServletRequest request) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		stakeholderBO = new StakeholderBO();
+		proxima = "stakeholder/listStakeholder.jsp";
+		
+		try {
+			List<Stakeholder> listaStakeholders = stakeholderBO.listarStakeholder();
+			request.setAttribute("listaStakeholders", listaStakeholders);
+		}catch (NegocioException e) {
+				request.setAttribute("msgErro", e.getMessage());
+			}
+			
+		return proxima;
 	}
 
 }
