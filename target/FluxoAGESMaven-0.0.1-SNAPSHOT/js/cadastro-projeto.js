b6f0e239e44d7e52e7b11b5b2d7ebf69e21b6029
Cassio Trindade <cassiowt@gmail.com>
2016-03-09 13:56:52 -0300

    Atualizado propriedades do pom.xml

--- target/FluxoAGESMaven-0.0.1-SNAPSHOT/js/cadastro-projeto.js
+++
@@ -1,9 +0,0 @@
-	$('#addUserBtn').click(function() {
-		var novoIntegrante = $('.addUser')[0].outerHTML;
-		$('#addUserSection').append(novoIntegrante);
-	});
-
-	$('#removeUserBtn').click(function() {
-
-		$('#addUserSection').find('.removeUser').parent().remove();
-	});
--- target/FluxoAGESMaven-0.0.1-SNAPSHOT/template/foot.jsp
+++
@@ -1,8 +0,0 @@
-    </div>
-</body>
-
-<footer>
-	<p style="text-align: center;" >&copy; Copyright AGES 2015 - Todos os direitos reservados</p>
-</footer>
-
-</html>
