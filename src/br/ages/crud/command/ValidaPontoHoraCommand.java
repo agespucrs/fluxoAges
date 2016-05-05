ca2a1b35e99a9b72ee470cd07aa9ed8ba07869f9
Matheus Vaccaro <matheus.vaccaro@acad.pucrs.br>
2016-05-05 16:08:56 -0300

    implementação do command de validação

--- src/br/ages/crud/command/ValidaPontoHoraCommand.java
+++ src/br/ages/crud/command/ValidaPontoHoraCommand.java
@@ -29,6 +29,11 @@ public class ValidaPontoHoraCommand implements Command {
 			String idResponsavel;
 			String senhaResponsavel;
 
+			idPonto = request.getParameter("id_ponto");
+			idResponsavel = request.getParameter("id_responsavel");
+			senhaResponsavel = request.getParameter("senha");
+			
+			
 			pontoBO = new PontoBO();
 			responsavelBO = new UsuarioBO();
 
@@ -41,12 +46,12 @@ public class ValidaPontoHoraCommand implements Command {
 				ponto.setResponsavel(responsavel);
 				ponto.setStatus(statusPonto);
 				pontoBO.editaPonto(ponto);
-				request.setAttribute("msgSucesso", MensagemContantes.MSG_SUC_PONTO);
+				request.setAttribute("msgSucesso", MensagemContantes.MSG_SUC_VALIDA_HORA_INVALIDA_PONTO);
 			} else {
-				request.setAttribute("msgErro", MensagemContantes.MSG_ERR_PONTO);
+				request.setAttribute("msgErro", MensagemContantes.MSG_ERR_VALIDA_HORA_INVALIDA_PONTO);
 			}
 
-		} catch (SQLException | NegocioException | ParseException | PersistenciaException e){
+		} catch (NegocioException | PersistenciaException e){
 			e.printStackTrace();
 		}
 		return null;
