918a919993c0b93915355d49f5e88daecfbf49ed
Matheus Vaccaro <matheus.vaccaro@acad.pucrs.br>
2016-05-06 16:49:40 -0300

    funciona, mas tem bugs

--- src/br/ages/crud/command/ValidaPontoHoraCommand.java
+++ src/br/ages/crud/command/ValidaPontoHoraCommand.java
@@ -22,7 +22,7 @@ public class ValidaPontoHoraCommand implements Command {
 
 	@Override
 	public String execute(HttpServletRequest request) {
-
+		proxima = "aluno/listPontoHoraInvalido.jsp";
 	try {
 
 			String idPonto;
@@ -54,7 +54,7 @@ public class ValidaPontoHoraCommand implements Command {
 		} catch (NegocioException | PersistenciaException e){
 			e.printStackTrace();
 		}
-		return null;
+		return proxima;
 	}
 
 }
